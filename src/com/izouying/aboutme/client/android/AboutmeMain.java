15b621756cf7324e3c4d0f9dbe6e873e835ac945
izouying <llin.zou@gmail.com>
2013-04-15 23:58:31 +0800

    添加制作QRcodeflagment
    添加制作QRcodeflagment

--- src/com/izouying/aboutme/client/android/AboutmeMain.java
+++ src/com/izouying/aboutme/client/android/AboutmeMain.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.*;
 import android.widget.*;
 import com.izouying.aboutme.client.android.addInfotools.ScanTabFragment;
+import com.izouying.aboutme.client.android.makeqrcode.MakeQRCodeFragment;
 import com.izouying.aboutme.client.android.showmyinfo.MyInfoFragment;
 
 /**
@@ -55,11 +56,12 @@ public class AboutmeMain extends Activity {
 
     void initActionBar(){
         mActionBar = getActionBar();
+        mActionBar.removeAllTabs();
         mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         mActionBar.setDisplayOptions(0);
-        mActionBar.setDisplayUseLogoEnabled(false);
-        mActionBar.setDisplayShowTitleEnabled(false);
+       // mActionBar.setDisplayUseLogoEnabled(false);
+       // mActionBar.setDisplayShowTitleEnabled(false);
 
 
         ScanTabFragment scanTabFragment = new ScanTabFragment(this);
@@ -69,7 +71,7 @@ public class AboutmeMain extends Activity {
         MyInfoFragment aboutmeFragment = new MyInfoFragment(getResources().getString(R.string.show_my_info));
         mActionBar.addTab(mActionBar.newTab().setText(R.string.show_my_info).setTabListener(new myTabListener(aboutmeFragment)));
 
-        TabContentFragment makeQRCodeFragment = new TabContentFragment(getResources().getString(R.string.make_qrcode));
+        MakeQRCodeFragment makeQRCodeFragment = new MakeQRCodeFragment(this);
         mActionBar.addTab(mActionBar.newTab().setText(R.string.make_qrcode).setTabListener(new myTabListener(makeQRCodeFragment)));
 
     }
@@ -98,24 +100,4 @@ public class AboutmeMain extends Activity {
         }
     }
 
-    private class TabContentFragment extends Fragment {
-        private String mText;
-        public TabContentFragment(String text) {
-            mText = text;
-        }
-        @Override
-        public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState) {
-            View fragView = layoutInflater.inflate(R.layout.actionbar_tab_content,container,false);
-            TextView textView = (TextView)fragView.findViewById(R.id.text);
-            textView.setText(mText);
-            return fragView;
-        }
-    }
-
-    private void makeQRCode(){
-        Intent intent = getIntent();
-        intent.setClass(AboutmeMain.this, MakeQRCodeActivity.class);
-        startActivity(intent);
-        Log.v(TAG, "Start makeQRCode activity!!!!!!!") ;
-    }
 }
